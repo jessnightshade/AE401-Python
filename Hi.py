c4faaee188b80d451980a2cfd4471a76854abce0
jessnightshade <60870195+jessnightshade@users.noreply.github.com>
2020-02-10 15:25:56 +0800

    hi talk to computer

---
+++ Hi.py
@@ -0,0 +1,19 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello world!")
+hello world!
+>>> print("I'm Eric")
+I'm Eric
+>>> print('He is "cool"')
+He is "cool"
+>>> a=input("name: ")
+name: Jessica
+>>> print("hi,"a)
+SyntaxError: invalid syntax
+>>> print("hi"+a)
+hiJessica
+>>> print("hi"+ a)
+hiJessica
+>>> print("hi "+a)
+hi Jessica
+>>> 

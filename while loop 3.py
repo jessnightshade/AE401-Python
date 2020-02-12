c7265604ecac5d1b3db455aefc9dd70eb7d22109
jessnightshade <60870195+jessnightshade@users.noreply.github.com>
2020-02-12 16:52:18 +0800

    how to use the while loop function

---
+++ while loop 3.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 16:34:48 2020
+
+@author: Jamie
+"""
+
+import random
+a=random.randint(1,20)
+i=0
+while i<5:
+    x=i
+    b=input('you have tried '+str(x)+' times. guess a number from 1-20:')
+    if int(b)==a:
+        print('right')
+        break
+    else:
+        print('try again') 
+    i=i+1    
+while i==5:
+    print('you fail')
+    

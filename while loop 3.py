a502cb1c9f87d1b5504adb29a2c21d3caaf71253
jessnightshade <60870195+jessnightshade@users.noreply.github.com>
2020-02-13 12:01:48 +0800

    Update while loop 3.py

--- while loop 3.py
+++ while loop 3.py
@@ -7,14 +7,17 @@ Created on Wed Feb 12 16:34:48 2020
 
 import random
 a=random.randint(1,20)
+print('you have 5 tries')
 i=1
 while i<6:
     b=input('guess a number from 1-20:')
     if int(b)==a:
-        print('right! you have tried '+str(x)+' times. ')
+        print('right! you have tried '+str(i)+' times. ')
         break
-    else:
-        print('wrong! you have tried '+str(x)+' times. ') 
+    elif int(b)<a:
+        print('wrong! you have tried '+str(i)+' times. try a higher number')
+    elif int(b)>a:
+        print('wrong! you have tried '+str(i)+' times. try a lower number number')
     i=i+1    
 if i==6:
     print('you fail')
